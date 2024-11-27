673adf3da5c4833dc63002ec1b05e0d0cbed31a3
blackjack <kurawata414@gmail.com>
2024-11-27 12:49:14 +0900

    🧱 デプロイコードを構築

--- main.tf
+++ main.tf
@@ -12,80 +12,71 @@ provider "aws" {
     region = "ap-northeast-1"
 }
 
-# lambda 用の S3 バケットを定義
-resource "aws_s3_bucket" "lambda_code" {
-    bucket = "lambda-code-bucket-unique-name-12345"
+# ECR リポジトリの作成
+resource "aws_ecr_repository" "practice_repo" {
+    name = "practice-docker-lambda-deploy"
+    image_tag_mutability = "MUTABLE"
     tags = {
-        Name = "Lambda Code Bucket"
+        Name = "Practice Docker Lambda Repository"
         Environment = "Practice"
     }
 }
 
-resource "aws_s3_bucket_public_access_block" "lambda_code_public_access_block" {
-    bucket = aws_s3_bucket.lambda_code.id
-    block_public_acls = true
-    block_public_policy = true
-    ignore_public_acls = true
-    restrict_public_buckets = true
+# 構築したリポジトリのURLを出力
+output "ecr_repository_url" {
+    value = aws_ecr_repository.practice_repo.repository_url
 }
 
-resource "aws_s3_object" "lambda_zip" {
-    bucket = aws_s3_bucket.lambda_code.bucket
-    key = "lambda/handler.zip"
-    source = "${path.module}/lambda/handler.zip"
-    tags = {
-        Name = "Lambda Handler Zip"
-        Environment = "Practice"
-    }
-}
-
-# Lambda に適用可能な IAM ロールを構築する
+# Lambda のロールを構築し、ポリシーをアタッチ
 resource "aws_iam_role" "lambda_role" {
-    name = "lambda_role"
+    name = "lambda_docker_role"
     assume_role_policy = jsonencode({
         Version = "2012-10-17"
         Statement = [
             {
-                Action = "sts:AssumeRole"
                 Effect = "Allow"
                 Principal = {
                     Service = "lambda.amazonaws.com"
                 }
+                Action = "sts:AssumeRole"
             }
         ]
     })
 }
 
-# 直前で構築された IAM ロールに対してポリシーを紐づけ
-# ここでは CloudWatch Logs に関連するアクションをいくつか許可
 resource "aws_iam_role_policy" "lambda_policy" {
     role = aws_iam_role.lambda_role.id
-
     policy = jsonencode({
         Version = "2012-10-17"
         Statement = [
             {
+                Effect = "Allow"
                 Action = [
                     "logs:CreateLogGroup",
                     "logs:CreateLogStream",
                     "logs:PutLogEvents"
                 ]
-                Effect = "Allow"
                 Resource = "arn:aws:logs:*:*:*"
+            },
+            {
+                Effect = "Allow"
+                Action = [
+                    "ecr:GetDownloadUrlForLayer",
+                    "ecr:BatchGetImage"
+                ]
+                Resource = aws_ecr_repository.practice_repo.arn
             }
         ]
     })
 }
 
-# Lambda 関数を定義
-resource "aws_lambda_function" "example" {
-    function_name = "example_lambda"
+resource "aws_lambda_function" "docker_lambda" {
+    function_name = "practice_docker_lambda"
     role = aws_iam_role.lambda_role.arn
-    handler = "handler.lambda_handler"
-    runtime = "python3.9"
+    package_type = "Image"
+    timeout = 300
 
-    s3_bucket = aws_s3_bucket.lambda_code.bucket
-    s3_key = aws_s3_object.lambda_zip.key
+    image_uri = "${aws_ecr_repository.practice_repo.repository_url}:latest"
 
     environment {
         variables = {
@@ -94,7 +85,7 @@ resource "aws_lambda_function" "example" {
     }
 
     tags = {
-        Name = "Example Lambda"
+        Name = "Practice Doker Lambda"
         Environment = "Practice"
     }
 }

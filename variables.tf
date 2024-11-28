8c976cff292c67fe93bdcb595b07e1ddbca4b28c
blackjack <kurawata414@gmail.com>
2024-11-28 13:23:43 +0900

    🙈 環境変数による variable の注入を実施

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "bucket_name" {
+    description = "The name of the S3 bucket"
+    type = string
+}
+
+variable "prefix" {
+    description = "The prefix in the S3 bucket"
+    type = string
+}
